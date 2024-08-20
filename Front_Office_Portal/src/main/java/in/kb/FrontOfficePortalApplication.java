191e94b7c9802c56ae7649e097ddc286e069aab6
kbaduwal <khakendra1999@gmail.com>
2024-08-20 19:48:19 +0545

    Project Setup with necessary dependency.

---
+++ Front_Office_Portal/src/main/java/in/kb/FrontOfficePortalApplication.java
@@ -0,0 +1,13 @@
+package in.kb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontOfficePortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontOfficePortalApplication.class, args);
+	}
+
+}
---
+++ Front_Office_Portal/src/test/java/in/kb/FrontOfficePortalApplicationTests.java
@@ -0,0 +1,13 @@
+package in.kb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontOfficePortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
