1dd022f59e9e621c95643601de41077a46f19b7c
kbaduwal <khakendra1999@gmail.com>
2024-08-21 23:14:39 +0545

    Dummy method for User Controller.

--- Front_Office_Portal/src/main/java/in/kb/controllers/UserController.java
+++ Front_Office_Portal/src/main/java/in/kb/controllers/UserController.java
@@ -1,4 +1,30 @@
 package in.kb.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class UserController {
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+
+    @GetMapping("/signup")
+    public String signUpPage() {
+        return "signup";
+    }
+
+    @GetMapping("/unlock")
+    public String unlockPage() {
+        return "unlock";
+    }
+    @GetMapping("/forgot")
+    public String forgotPwdPage() {
+        return "forgotPwd";
+    }
+
+
+
 }
