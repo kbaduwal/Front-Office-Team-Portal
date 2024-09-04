d6d51a53f90fe34d19ce7394ddab4d2017a4494a
kbaduwal <khakendra1999@gmail.com>
2024-09-04 21:20:24 +0545

    Adding the local file

--- Front_Office_Portal/src/main/java/in/kb/controllers/EnquiryController.java
+++ Front_Office_Portal/src/main/java/in/kb/controllers/EnquiryController.java
@@ -1,4 +1,23 @@
 package in.kb.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class EnquiryController {
+
+    @GetMapping("/dashboard")
+    public String dashboardPage(){
+        return "dashboard";
+    }
+
+    @GetMapping("/enquiry")
+    public String addEnquiryPage(){
+        return "add-enquiry";
+    }
+
+    @GetMapping("/enquiries")
+    public String viewEnquiriesPage(){
+        return "view-enquiries";
+    }
 }
