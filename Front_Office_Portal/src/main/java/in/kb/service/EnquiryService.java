59e4034ca1fcb110099222c55e9a795e7513fd20
kbaduwal <khakendra1999@gmail.com>
2024-09-10 20:38:57 +0530

    added abstract method for Enquiry Service

--- Front_Office_Portal/src/main/java/in/kb/service/EnquiryService.java
+++ Front_Office_Portal/src/main/java/in/kb/service/EnquiryService.java
@@ -1,4 +1,15 @@
 package in.kb.service;
 
+import in.kb.binding.DashBoardResponse;
+import in.kb.binding.EnquiryForm;
+import in.kb.binding.EnquirySearchCriteria;
+
+import java.util.List;
+
 public interface EnquiryService {
+    public List<String> getCourseName();
+    public List<String> getEnqStatus();
+    public DashBoardResponse getDashboardData(Integer userId);
+    public String addEnquiry(EnquiryForm form);
+    public List<EnquiryForm> getEnquiries(Integer userID, EnquirySearchCriteria criteria);
 }
